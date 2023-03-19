2188cd4266da90bd82cd2f68daa4c89a64deb3ed
palisthadeshar <67106733+palisthadeshar@users.noreply.github.com>
2023-03-20 00:23:34 +0545

    Update MainActivity.kt

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -11,9 +11,6 @@ import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
-    private var tipAmt:Double=0.0
-    private var isSwitchChecked:Boolean=true
-    private var totalTip:Double=0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
